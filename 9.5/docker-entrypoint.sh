c66185afe18047a253caff9ec8aa6fc74a661be3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-08-02 10:24:17 +0200

    Use "pghoard" for replicate slot name

--- 9.5/docker-entrypoint.sh
+++ 9.5/docker-entrypoint.sh
@@ -8,7 +8,7 @@ until psql -qAt -U replicator -h $PG_HOST -d postgres -c "select user;"; do
   echo "sleep 1s and try again ..."
   sleep 1
 done
-psql -h $PG_HOST -c "SELECT * FROM pg_create_physical_replication_slot('${HOSTNAME}');" -U replicator -d postgres
+psql -h $PG_HOST -c "SELECT * FROM pg_create_physical_replication_slot('pghoard');" -U replicator -d postgres
 
 echo "Create pghoard configuration with confd ..."
 confd -onetime -backend env

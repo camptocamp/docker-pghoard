229c080a66993eb73980f3a7c2ada0508f0a53fa
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-05-18 16:56:23 +0200

    Use $PG_USER instead of replicator

--- 9.6/docker-entrypoint.sh
+++ 9.6/docker-entrypoint.sh
@@ -19,11 +19,11 @@ if [ -z "${PGHOARD_RESTORE_SITE}" ]; then
 
   echo "Create physical_replication_slot on master ..."
   export PGPASSWORD=$PG_PASSWORD
-  until psql -qAt -U replicator -h $PG_HOST -d postgres -c "select user;"; do
+  until psql -qAt -U $PG_USER -h $PG_HOST -d postgres -c "select user;"; do
     echo "sleep 1s and try again ..."
     sleep 1
   done
-  psql -h $PG_HOST -c "WITH foo AS (SELECT COUNT(*) AS count FROM pg_replication_slots WHERE slot_name='pghoard') SELECT pg_create_physical_replication_slot('pghoard') FROM foo WHERE count=0;" -U replicator -d postgres
+  psql -h $PG_HOST -c "WITH foo AS (SELECT COUNT(*) AS count FROM pg_replication_slots WHERE slot_name='pghoard') SELECT pg_create_physical_replication_slot('pghoard') FROM foo WHERE count=0;" -U $PG_USER -d postgres
 
   echo "Run the pghoard daemon ..."
   exec gosu postgres pghoard --short-log --config /home/postgres/pghoard.json

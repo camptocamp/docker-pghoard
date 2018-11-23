0775774fb8bfa5183a1c3945a61a6bacb836445b
Cédric de Saint Martin <cdesaintmartin@wiremind.fr>
2018-11-23 10:58:52 +0100

    Create make script.

---
+++ README.md
@@ -0,0 +1,18 @@
+# How to generate dockerfiles
+
+## Requirements
+
+- envsubst from GNU gettext
+
+### Mac Installation through Homebrew
+
+```bash
+$ brew install gettext
+$ brew link --force gettext
+```
+
+## Generate
+
+```bash
+./make.sh
+```
---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script will generate the Dockerfiles
+set -e
+
+cd $(dirname $0)
+
+for VERSION in "9.5" "9.6"; do
+    mkdir -p $VERSION
+    cp -r templates/files/* $VERSION
+    sed -e 's/\$DOCKER_POSTGRES_VERSION/'$VERSION'/g' "templates/Dockerfile-postgresql-9.template" > $VERSION/Dockerfile
+done
+
+for VERSION in "10"; do
+    mkdir -p $VERSION
+    cp -r templates/files/* $VERSION
+    sed -e 's/\$DOCKER_POSTGRES_VERSION/'$VERSION'/g' "templates/Dockerfile-postgresql.template" > $VERSION/Dockerfile
+done

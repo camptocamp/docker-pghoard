9f96a6b993332f8b9a87bb5509cc89f902dd0efd
Julien Acroute <julien.acroute@camptocamp.com>
2019-09-23 09:19:46 +0200

    Remove credentials from config before display it

--- 10/generate_config.sh
+++ 10/generate_config.sh
@@ -16,6 +16,11 @@ else
   echo "$0: Configuration already present"
 fi
 
-echo "$0: Dump configuration... (password removed)"
-cat $PGHOARD_CONFIG | grep -v 'password' | grep -v 'secret'
+echo "$0: Dump configuration... (credentials removed)"
+RAW_CONFIG=$(cat $PGHOARD_CONFIG)
+KEY_TO_REMOVE="nodes[].password encryption_keys[].private object_storage.aws_secret_access_key object_storage.password object_storage.private_key object_storage.credential_file object_storage.account_key object_storage.key"
+for key in $KEY_TO_REMOVE; do
+  RAW_CONFIG=$(echo "$RAW_CONFIG" | jq "del(.backup_sites[].$key)")
+done
+echo $RAW_CONFIG | jq
 exit 0

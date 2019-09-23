9de4c34a2aa4913f3f60be9bc17ae08d05dc1ef8
Julien Acroute <julien.acroute@camptocamp.com>
2019-09-23 09:53:09 +0200

    Fix display of configuration

--- 10/generate_config.sh
+++ 10/generate_config.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Listing files in /etc/pghoard/"
-ls -l /etc/pghoard/
-
 echo "$0: PGHoard config : $PGHOARD_CONFIG"
 
 if [ ! -r $PGHOARD_CONFIG ]; then
@@ -17,10 +14,14 @@ else
 fi
 
 echo "$0: Dump configuration... (credentials removed)"
+# Remove credentials from config
 RAW_CONFIG=$(cat $PGHOARD_CONFIG)
 KEY_TO_REMOVE="nodes[].password encryption_keys[].private object_storage.aws_secret_access_key object_storage.password object_storage.private_key object_storage.credential_file object_storage.account_key object_storage.key"
 for key in $KEY_TO_REMOVE; do
-  RAW_CONFIG=$(echo "$RAW_CONFIG" | jq "del(.backup_sites[].$key)")
+  RAW_CONFIG_TMP=$(echo "$RAW_CONFIG" | jq "del(.backup_sites[].$key)" 2> /dev/null)
+  if [ -n "$RAW_CONFIG_TMP" ]; then
+    RAW_CONFIG=$RAW_CONFIG_TMP
+  fi
 done
 echo $RAW_CONFIG | jq
 exit 0
